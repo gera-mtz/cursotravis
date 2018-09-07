d17dd1e2160d26822678f26808a1d5bd6fd48c15
gera-mtz <geramtz100@gmail.com>
2018-09-07 17:34:15 -0500

    [ADD]hello.py : add file in travis

---
+++ hello.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+suma = 4 + 6
+
+def resultado(suma):
+	if(suma == 10):
+		print('la suma es %s') % str(suma)
+	else:
+		print('la suma no es igual a %s') %str(suma)
+	return suma

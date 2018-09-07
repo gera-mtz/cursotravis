91a18cb3b85224901b45b10d13c9b03ffacf8769
gera-mtz <geramtz100@gmail.com>
2018-09-07 17:48:44 -0500

    errores mensos

---
+++ hello2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+suma === 4 + 6
+
+def resultado(suma)
+	if(suma == 10)
+		print('''la suma es %''') % str(suma)
+	else
+		print('la suma no es igual a %') %str(suma)
+	return suma
